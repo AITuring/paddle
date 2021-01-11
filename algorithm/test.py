106babcc635261398544bf4a20b85a68f50af82e
lishengyu <shengyu.li@seetatech.com>
2021-01-11 10:30:00 +0800

    添加后续章节

--- algorithm/test.py
+++ algorithm/test.py
@@ -1,24 +1,12 @@
-def kClosest(points, K):
-    distance={}
-    for i in range(len(points)):
-        # print(points[i][0]**2,points[i][1]**2,points[i][0]**2+points[i][1]**2)
-        distance[i] = points[i][0]**2+points[i][1]**2
-    print(distance)
-    s=sorted(distance.items(), key=lambda x: x[1])
-    # print(s[K-1][0])
-    # t=s[K-1][0]
-    ans=[]
-    for m in range(1,K+1):
-        print(m)
-        print(points[s[m - 1][0]])
-        ans.append(points[s[m - 1][0]])
+def monotoneIncreasingDigits(N: int) -> int:
+    ones = 111111111
+    result = 0
+    for _ in range(9):
+        print(ones,result,_)
+        while result + ones > N:
+            ones //= 10
+        result += ones
+    return result
 
-    print(ans)
-    return ans
-    # print(points[s[K-1][0]])
-    return points[s[K-1][0]]
-points = [[1,3],[-2,2]]
-K = 1
-# points = [[3,3],[5,-1],[-2,4]]
-# K = 2
-kClosest(points, K)
+N=332
+print(monotoneIncreasingDigits(N))
